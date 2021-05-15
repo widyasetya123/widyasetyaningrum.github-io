db1015a65503b4f05c34c458fc228095d2889369
widyasetya123 <72067963+widyasetya123@users.noreply.github.com>
2021-05-15 20:03:05 +0700

    Create home.php

---
+++ quizpemroweb/home.php
@@ -0,0 +1,6 @@
+<p></p>
+<body>
+    <center><font size="+3" face="Showcard Gothic">SELAMAT DATANG</font></center>
+    <center><font size="+4" face="arial">MY LIBRARY</font></center>
+    <center><font size="+7" face="arial"><b>BUKU MERUPAKAN JENDELA DUNIA</b></font></center>
+</body>
